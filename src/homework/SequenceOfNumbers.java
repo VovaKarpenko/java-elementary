c4efc0d6bd907d71495ae5204ba48150d8236ee0
vovakarpenko <vovakarpenko781@gmail.com>
2022-03-23 22:53:05 +0200

    homework 2

--- src/homework/SequenceOfNumbers.java
+++ src/homework/SequenceOfNumbers.java
@@ -11,16 +11,15 @@ public class SequenceOfNumbers {
 
     }
 
-    public static void sequenceOfNumbers(String strArr) {
+    public static boolean sequenceOfNumbers(String strArr) {
         int[] numArr = Arrays.stream(strArr.split(" ")).mapToInt(Integer::parseInt).toArray();
         for (int i = 0; i < numArr.length - 1; i++) {
             if (numArr[i] >= numArr[i + 1]) {
                 System.out.println("не является строго возростающей");
-                break;
-            }
-            if (i == numArr.length-2){
-                System.out.println("является строго возростающей");
+                return false;
             }
         }
+        System.out.println("является строго возростающей");
+        return true;
     }
 }
