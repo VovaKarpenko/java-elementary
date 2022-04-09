c68e682017dcaebaabb891a3c9003ab77ac65ff2
vovakarpenko <vovakarpenko781@gmail.com>
2022-04-09 10:14:44 +0300

    homework 8

--- src/homework/homework8/Person.java
+++ src/homework/homework8/Person.java
@@ -4,6 +4,10 @@ public class Person {
 
     String name;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
     class Account {
         int password;
 
--- src/homework/homework8/PersonMain.java
+++ src/homework/homework8/PersonMain.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class PersonMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Person person = new Person();
-
         System.out.print("Введите имя пользователя: ");
-        person.name = scanner.nextLine();
+        Person person = new Person(scanner.nextLine());
 
         System.out.print("Введите пороль: ");
         Person.Account account = person.new Account(scanner.nextInt());
