60c155098da3cd2488c8c8e47707b89fd21ea8e2
vovakarpenko <vovakarpenko781@gmail.com>
2022-05-14 12:48:24 +0300

    homework 14 2.0

--- src/homework/homework14/LikeArraylist.java
+++ src/homework/homework14/LikeArraylist.java
@@ -2,17 +2,29 @@ package homework.homework14;
 
 public class LikeArraylist {
 
-    String[] array = new String[0];
+    String[] array = new String[10];
     String[] bufferArray = new String[0];
+    private int size =0;
 
-    public void add(String word) {
-        bufferArray = new String[array.length + 1];
-        for (int i = 0; i < array.length; i++) {
-            bufferArray[i] = array[i];
+    public void grow(int size) {
+        if (size%10==0){
+            bufferArray = new String[array.length + 10];
+            for (int i = 0; i < array.length; i++) {
+                bufferArray[i] = array[i];
+            }
+            array = bufferArray;
+
         }
-        array = bufferArray;
-        array[array.length - 1] = word;
+
     }
+    public void add (String word){
+        grow(size+1);
+        array[size]=word;
+        size++;
+    }
+
+
+
 
     public void remove(int index) {
         bufferArray = new String[array.length - 1];
@@ -43,12 +55,20 @@ public class LikeArraylist {
     public String toStrin() {
         String result = "[";
         for (int i = 0; i < array.length; i++) {
+            if (array[i]==null){
+                break;
+            }
             result += array[i];
-            if (i < array.length - 1)
+
+            if (!(array[i+1]==null)) {
                 result += " , ";
+            }
+
         }
         result += "]";
         return result;
     }
+
+
 
 }
--- src/homework/homework14/Main.java
+++ src/homework/homework14/Main.java
@@ -6,6 +6,15 @@ public class Main {
         arr.add("a");
         arr.add("b");
         arr.add("c");
+        arr.add("1");
+        arr.add("2");
+        arr.add("3");
+        arr.add("4");
+        arr.add("5");
+        arr.add("6");
+        arr.add("7");
+
+
         System.out.println(arr.toStrin());
 
         arr.set(1, "x");
