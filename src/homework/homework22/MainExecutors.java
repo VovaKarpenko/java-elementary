572c935cc5588da0c8850ae0cfc2ba03455116b2
vovakarpenko <vovakarpenko781@gmail.com>
2022-06-18 12:21:57 +0300

    homework 22.0

--- src/homework/homework22/MainExecutors.java
+++ src/homework/homework22/MainExecutors.java
@@ -1,6 +1,8 @@
 package homework.homework22;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 
@@ -11,16 +13,22 @@ public class MainExecutors {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Future<Integer>> arr = new ArrayList<>();
+
 
         for (int i = 0; i < 10; i++) {
-            COUNT += executor.submit(new Count()).get();
+            arr.add(executor.submit(new Count()));
+        }
+        executor.shutdown();
+        for (Future<Integer> ex : arr) {
+            COUNT += ex.get();
         }
 
         System.out.println(COUNT);
     }
 
     public static Integer counter(Integer plusOne) {
-     return ++plusOne;
+        return ++plusOne;
     }
 
     public static class Count implements Callable<Integer> {
@@ -30,7 +38,7 @@ public class MainExecutors {
 
             Integer buff = 0;
             for (int i = 0; i < 1000; i++) {
-               buff = counter(buff);
+                buff = counter(buff);
             }
             System.out.println(Thread.currentThread().getName());
             return buff;
